20fef3aa4c82f01661043d6d6e583ce3cc60f855
unknown <bbabines@gmail.com>
2023-01-01 21:13:03 -0500

    Fixed styling clashes and dynamic nested routing

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -16,6 +16,8 @@ import {
 	setDoc,
 	collection,
 	writeBatch,
+	query,
+	getDocs,
 } from "firebase/firestore";
 
 const firebaseConfig = {
@@ -57,6 +59,21 @@ export const addCollectionAndDocuments = async (
 
 	await batch.commit();
 	console.log("done");
+};
+
+// To prevent code breaking changes from third parties e.g. firebase.
+export const getCategoriesAndDocuments = async () => {
+	const collectionRef = collection(db, "categories");
+	const q = query(collectionRef);
+
+	const querySnapshot = await getDocs(q);
+	const categoryMap = querySnapshot.docs.reduce((acc, docSnapShot) => {
+		const { title, items } = docSnapShot.data();
+		acc[title.toLowerCase()] = items;
+		return acc;
+	}, {});
+
+	return categoryMap;
 };
 
 export const createUserDocumentFromAuth = async (
